50ce28f144aad88d1757399c3e5d5a8018f7fd8f
Primakov Nikita <97438140+NikitaPrimakov@users.noreply.github.com>
2024-12-12 14:43:57 +0500

    update 3-day-practice

--- Python/3-day-practice.py
+++ Python/3-day-practice.py
@@ -69,3 +69,99 @@ if (intTwo - intOne) == (intThree - intTwo):
     print("YES")
 else:
     print("NO")
+    
+
+# Задача 6
+
+# Напишите программу, которая определяет наименьшее из двух чисел.
+
+intOne = int(input('Введите число №1: '))
+intTwo = int(input('Введите число №2: '))
+
+if intOne > intTwo:
+    print(intTwo)
+else:
+    print(intOne)
+    
+    
+# Задача 7
+
+# Напишите программу, которая определяет наименьшее из четырёх чисел.
+
+intOne = int(input('Введите число №1: '))
+intTwo = int(input('Введите число №2: '))
+intThree = int(input('Введите число №3: '))
+intFour = int(input('Введите число №4: '))
+
+minNumOne = 0
+minNumTwo = 0
+
+if intOne < intTwo:
+    minNumOne = intOne
+else:
+    minNumOne = intTwo
+    
+if intThree < intFour:
+    minNumTwo = intThree
+else:
+    minNumTwo = intFour
+    
+if minNumOne < minNumTwo or minNumOne == minNumTwo:
+    print(minNumOne)
+else:
+    print(minNumTwo)
+
+
+# Задача 8
+
+# Напишите программу, которая по введённому возрасту пользователя сообщает, к какой возрастной группе он относится: 
+# до 13 (включительно) – детство; 
+# от  14 до 24 (включительно) – молодость;от 25 до 59 (включительно) – зрелость; от 60 (включительно) – старость.
+
+ageOfPerson = int(input("Введите свой возраст: "))
+
+if ageOfPerson <= 13:
+    print("детство")
+if 14 <= ageOfPerson <= 24:
+    print("молодость")
+if 25 <= ageOfPerson <= 59:
+    print("зрелость")
+if ageOfPerson >= 60:
+    print("старость")
+    
+# Задача 9
+
+# Напишите программу, которая считывает три числа и подсчитывает сумму только положительных чисел.
+
+intOne = int(input('Введите число №1: '))
+intTwo = int(input('Введите число №2: '))
+intThree = int(input('Введите число №3: '))
+
+if intOne > 0 and intTwo < 0 and intThree < 0:
+    print(intOne)
+if intOne > 0 and intTwo > 0 and intThree < 0:
+    print(intOne + intTwo)
+if intOne > 0 and intTwo > 0 and intThree > 0:
+    print(intOne + intTwo + intThree)
+if intOne < 0 and intTwo > 0 and intThree < 0:
+    print(intTwo)
+if intOne < 0 and intTwo < 0 and intThree > 0:
+    print(intThree)
+if intOne > 0 and intTwo < 0 and intThree > 0:
+    print(intOne + intThree)
+if intOne < 0 and intTwo > 0 and intThree > 0:
+    print(intTwo + intThree)
+if intOne < 0 and intTwo < 0 and intThree < 0:
+    print(0)
+    
+
+# Альтернатива
+a, b, c = int(input()), int(input()), int(input())
+total = 0
+if a > 0:
+    total = total + a
+if b > 0:
+    total = total + b
+if c > 0:
+    total = total + c
+print(total)
